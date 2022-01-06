79141745ba39e4b071ca5ee823159a8619e6c644
Akim Benchiha <akim.benchiha@gmail.com>
2022-01-06 20:23:33 +0100

    Add Experience title

--- components/Experience/Experience.js
+++ components/Experience/Experience.js
@@ -1,9 +1,10 @@
 import React from "react";
+import { ExperienceContainer } from "./Experience.styles";
 function Experience(props, ref) {
   return (
-    <section id={props.id} ref={ref} style={{ height: "100vh" }}>
-      <p>Experience section</p>
-    </section>
+    <ExperienceContainer id={props.id} ref={ref}>
+      <h1 className="title-count">Experiences</h1>
+    </ExperienceContainer>
   );
 }
 
