88fe3887c20f94a00a877176851eff352b7d0df1
Akim Benchiha <akim.benchiha@gmail.com>
2022-01-08 21:39:37 +0100

    Fix footer on mobile

--- components/Footer/Footer.styles.js
+++ components/Footer/Footer.styles.js
@@ -32,14 +32,45 @@ export const FooterLink = styled.a`
 
 export const FooterSocialContainer = styled.div`
   display: flex;
-  width: 100%;
+  /*width: 100%;
   max-width: 250px;
   justify-content: space-between;
-  margin: 0px auto 10px;
+  margin: 0px auto 10px;*/
+
+  justify-content: space-around;
+  flex-wrap: wrap;
+  width: 75%;
+  margin: auto;
   svg {
     height: fit-content;
     width: fit-content;
     fill: white;
+  }
+  button,
+  a {
+    position: relative;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    &:hover {
+      outline: 0;
+      &:before {
+        transform: scale(1.2);
+        filter: blur(3px);
+      }
+    }
+
+    &:before {
+      position: absolute;
+      content: "";
+      top: 0;
+      left: 0;
+      height: 100%;
+      width: 100%;
+      border-radius: 50%;
+      z-index: -12;
+      transform: scale(0.9);
+    }
   }
   @media ${breakpoints.media_screens.landscape_tablet} {
     display: none;
