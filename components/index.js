6dc57bcff9d9875cf291fcc6a4603eda23fb35bd
Akim Benchiha <akim.benchiha@gmail.com>
2022-01-09 23:01:41 +0100

    Fix typo

--- components/index.js
+++ components/index.js
@@ -9,4 +9,4 @@ export { default as SocialMedia } from "./SocialMedia/SocialMedia";
 export { default as CustomRight } from "./CustomRight/CustomRight";
 export { default as Something } from "./Something/Something";
 export { default as SEO } from "./SEO/SEO";
-export { default as Feeds } from "./FeedBack/Feeds";
+export { default as Feeds } from "./Feedback/Feeds";
