8efa4b136bc9e28ce2e687293f6417d391468996
Akim Benchiha <akim.benchiha@gmail.com>
2022-01-10 09:38:28 +0100

    Add link to feedback from the something component

--- components/Something/Something.js
+++ components/Something/Something.js
@@ -1,23 +1,28 @@
 import akimHey from "@/public/assets/bitmojis/bitmoji-cactus.png";
 import Image from "next/image";
+import Link from "next/link";
 import { BitmojiContainer, SomethingContainer } from "./Something.styles";
 function Something() {
   return (
-    <SomethingContainer>
-      <div className="something-bubble">
-        Psst... Yes you ! Come over here. I have something for you.
-      </div>
-      <BitmojiContainer>
-        <Image
-          src={akimHey}
-          alt="Akim has something for your"
-          loading="lazy"
-          layout="responsive"
-          width={398}
-          height={398}
-        />
-      </BitmojiContainer>
-    </SomethingContainer>
+    <Link href="/feedback">
+      <a aria-label="Give feedback">
+        <SomethingContainer>
+          <div className="something-bubble">
+            Psst... Yes you ! Come over here. I have something for you.
+          </div>
+          <BitmojiContainer>
+            <Image
+              src={akimHey}
+              alt="Akim has something for your"
+              loading="lazy"
+              layout="responsive"
+              width={398}
+              height={398}
+            />
+          </BitmojiContainer>
+        </SomethingContainer>
+      </a>
+    </Link>
   );
 }
 
